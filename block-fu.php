a39dcd95d0fe594eb89150d57dc332af8f1bc4fc
John Dillick <john.dillick@highlights.com>
2014-05-05 09:48:12 -0400

    Rename some files to make clear what they are. Remove stunted script.

--- block-fu.php
+++
@@ -1,5 +0,0 @@
-<?php
-foreach ( system_region_list('stenhouse_theme') as $region_key => $region_name ) {
-  // print_r(block_list($region_key));
-  // print_r(block_get_blocks_by_region($region_key));
-}
