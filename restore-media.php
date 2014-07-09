1240556dd95aaa41430fdbd2cb93592fd7a898b4
John Dillick <john.dillick@highlights.com>
2014-07-09 10:29:07 -0400

    Niceties.

--- restore-media.php
+++ restore-media.php
@@ -1,28 +1,42 @@
 <?php
 
-$fields = db_select('field_config', 'fc')
-  ->condition('fc.type', 'image', '=')
-  ->fields('fc', array('field_name'))
-  ->execute()
-  ->fetchAll(PDO::FETCH_COLUMN);
+function restore_missing_media() {
+  $count = 0;
+  $files = get_managed_image_files();
+  $totalsize = get_total_size($files);
+  foreach ( $files as $file ) {
+    $url = file_create_url($file->uri);
+    $file_name = drupal_realpath($file->uri);
+    if ($file_url && $file_name) replace_media($file_url, $file_name);
 
-$fids = array();
-foreach ( $fields as $field ) {
-  $fids = array_merge(db_select('field_data_' . $field, 'fd')
+    display_media_progress($count++, $file, $files);
+  }
+}
+
+function get_managed_image_files() {
+  $fields = db_select('field_config', 'fc')
+    ->condition('fc.type', 'image', '=')
+    ->fields('fc', array('field_name'))
+    ->execute()
+    ->fetchAll(PDO::FETCH_COLUMN);
+
+  $fids = array();
+  foreach ( $fields as $field ) {
+    $fids = array_merge(db_select('field_data_' . $field, 'fd')
       ->fields('fd', array($field . '_fid'))
       ->execute()
       ->fetchAll(PDO::FETCH_COLUMN), $fids);
+  }
+
+  return file_load_multiple($fids);
 }
 
-$files = file_load_multiple($fids);
-
-$count = 0;
-foreach ( $files as $file ) {
-  $url = file_create_url($file->uri);
-  $file = drupal_realpath($file->uri);
-  if ($url && $file) replace_media($url, $file);
-
-  echo floor($count++ / count($files) * 100) . "% Complete\r";
+function get_total_size($files) {
+  $size = 0;
+  foreach ( $files as $file ) {
+    $size += $file->filesize;
+  }
+  return $size;
 }
 
 function replace_media ($url, $file) {
@@ -42,3 +56,23 @@ function replace_media ($url, $file) {
   }
 }
 
+function display_media_progress($count, $file, $files) {
+  static $size_so_far = 0;
+  $size_so_far += $file->filesize;
+  $totalsize = get_total_size($files);
+  $percentage = floor($count / count($files) * 100);
+
+  echo '[';
+  for($i = 1; $i <= 10; $i++) {
+    if($i <= ($percentage / 10)) echo "#";
+    else echo " ";
+  }
+
+  $kb = 1024;
+  $mb = $kb * 1024;
+  $unit = $totalsize > $mb ? $mb : $kb;
+  $unit_label = $totalsize > $mb ? 'M' : 'K';
+  echo sprintf("] (%d%% %d of %d%s downloaded)\r", $percentage, $size_so_far / $unit, $totalsize / $unit, $unit_label);
+}
+
+restore_missing_media();
