930efa9b9fca9d6ca93a52502d62984e216eb16a
John Dillick <john@dillick.us>
2014-08-20 11:27:05 -0400

    Made panels-import a bit more portable, by inserting new nodes / displays / panes, and mapping nested panels.

--- panels-import.php
+++ panels-import.php
@@ -1,4 +1,9 @@
 <?php
+namespace hfc\panels_import;
+use \GlobIterator;
+
+module_load_include('inc', 'ctools', 'includes/export');
+
 $args = drush_get_arguments();
 
 if ( ! isset($args[2]) ) {
@@ -11,41 +16,83 @@ if ( ! is_dir($args[2]) ) {
   exit();
 }
 
-foreach ( new GlobIterator(realpath($args[2]) . "/panel*.inc") as $panel_import ) {
-  include_once $panel_import->getPathname();
+import($args[2]);
 
-  $existing_display = db_select('panels_display','d')
-    ->fields('d', array('did'))
-    ->condition('did', $display['did'], '=')
-    ->range(0,1)
-    ->execute()
-    ->fetch();
-  if ( ! $existing_display ) {
-    drupal_set_message(t('Inserting panels display @did', array('@did'=>$display['did'])));
-    db_insert('panels_display')->fields($display)->execute();
-  } else {
-    drupal_set_message(t('Updating panels display @did', array('@did'=>$display['did'])));
-    db_update('panels_display')->fields($display)->condition('did', $display['did'], '=')->execute();
+function import($path) {
+  $nodes = array();
+  $displays = array();
+  $node_panes = array();
+  $mappings = array();
+
+  foreach ( new GlobIterator(realpath($path) . "/panel*.inc") as $panel_import ) {
+    include_once $panel_import->getPathname();
+    $nodes[$node->nid] = $node;
+    $displays[$node->nid] = $display;
+    $node_panes[$node->nid] = $panes;
   }
 
-  $existing_panes = db_select('panels_pane', 'p')
-    ->fields('p')
-    ->condition('p.pid', array_keys($panes), 'IN')
-    ->condition('p.did', $display['did'], '=')
-    ->execute()
-    ->fetchAllAssoc('pid', PDO::FETCH_ASSOC);
+  make_nodes_portable($nodes, $displays);
+  save_panels_nodes($nodes, $mappings);
+  map_panes($node_panes, $mappings, $nodes);
+  save_panels_pane($node_panes);
+}
 
-  foreach ( $panes as $pid => $pane ) {
-    if ( ! in_array($pid, array_keys($existing_panes)) ) {
-      drupal_set_message(t('Inserting panels pane @pid', array('@pid'=>$pid)));
-      db_insert('panels_pane')->fields($pane)->execute();
-    } else {
-      drupal_set_message(t('Updating panels pane @pid', array('@pid'=>$pid)));
-      db_update('panels_pane')->fields($pane)->condition('pid', $pid, '=')->execute();
+function make_nodes_portable(&$nodes, &$displays) {
+  foreach ( $nodes as $nid => $node ) {
+    unset(
+      $node->nid,
+      $node->vid,
+      $node->panels_node['did'],
+      $node->panels_node['nid'],
+      $node->path['pid'],
+      $node->path['source'],
+      $node->workbench_moderation
+    );
+
+    $display = $displays[$nid];
+    $display['did'] = 'new';
+    $node->export_display = '$display = ' . ctools_var_export((object) $display) . ';';
+  }
+}
+
+function save_panels_nodes(&$nodes, &$mappings) {
+  foreach ( $nodes as $nid => $node ) {
+    node_save($node);
+    $mappings[$nid] = $node->nid;
+    drupal_set_message(t('Saving panels node Source: @snid Target: @tnid', array(
+      '@snid'=>$nid,
+      '@tnid'=>$node->nid,
+    )));
+  }
+}
+
+function map_panes(&$node_panes, &$mappings, $nodes) {
+  foreach ( $node_panes as $nid => &$panes) {
+    foreach ( $panes as $pid => &$pane ) {
+      $pane['pid'] = 0;
+      $pane['did'] = $nodes[$nid]->panels_node['did'];
+
+      // fix nids
+      if ( $pane['type'] == 'node' ) {
+        $config = unserialize($pane['configuration']);
+        if ( in_array($config['nid'], array_keys($mappings)) ) {
+          $config['nid'] = $mappings[$config['nid']];
+          $pane['configuration'] = serialize($config);
+        }
+      }
     }
   }
+}
 
-  // insert or update node
-  drupal_set_message(t('Saving panels node @nid', array('@nid'=>$node->nid)));
-  node_save($node);
+function save_panels_pane($node_panes) {
+  foreach ( $node_panes as $nid => $panes ) {
+    foreach ( $panes as $pid => $pane ) {
+      $new_pid = db_insert('panels_pane')->fields($pane)->execute();
+      drupal_set_message(t('Inserting panels source nid: @nid source pane: @spid target pane: @tpid', array(
+        '@nid'=>$nid,
+        '@spid'=>$pid,
+        '@tpid'=>$new_pid,
+      )));
+    }
+  }
 }
