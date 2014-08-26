092f9abeabd6b6eda288a776cc05e526a9353bd0
John Dillick <john@dillick.us>
2014-08-26 16:40:35 -0400

    Menu Links Import/Export

---
+++ menu-links-export.php
@@ -0,0 +1,44 @@
+<?php
+
+module_load_include('inc', 'ctools', 'includes/export');
+
+$args = drush_get_arguments();
+if ( ! isset($args[2]) || ! isset($args[3]) ) {
+  drush_set_error('Usage: drush @<alias> scr menu-links-export.php <menu-names> <output-directory>');
+  exit();
+}
+
+if ( ! is_dir(realpath($args[3])) ) {
+  drush_set_error(t("Output directory @op does not exist.", array('@op' => $args[3])));
+  exit();
+}
+
+$menus = array();
+if (strpos($args[2], ',')) {
+  $menus = explode(',', $args[2]);
+  foreach ( $menus as $index => $nid ) {
+    $menus[$index] = trim($nid);
+  }
+}
+else {
+  $menus[] = trim($args[2]);
+}
+$menus = array_filter($menus);
+
+$menu_links = db_select('menu_links', 'm')
+  ->fields('m', array('link_path','link_title','menu_name','weight','expanded','options','mlid','plid','router_path'))
+  ->condition('m.menu_name', $menus, 'IN')
+  ->orderBy('depth','ASC')
+  ->execute()
+  ->fetchAllAssoc('mlid', PDO::FETCH_ASSOC);
+
+foreach ( $menu_links as $mlid => &$menu_link ) {
+  if ( strpos($menu_link['link_path'], 'taxonomy/term/') === 0 ||
+    strpos($menu_link['link_path'], 'node/') === 0 ) {
+    $menu_link['alias'] = drupal_get_path_alias($menu_link['link_path']);
+  }
+}
+
+$menu_links_file = "<?php\n";
+$menu_links_file .= '$menu_links = ' . ctools_var_export($menu_links) . ";\n";
+file_put_contents(realpath($args[3]) . '/' . 'menu-links.inc', $menu_links_file);
---
+++ menu-links-import.php
@@ -0,0 +1,108 @@
+<?php
+namespace hfc\menu_links_import;
+use \GlobIterator;
+
+module_load_include('inc', 'ctools', 'includes/export');
+
+$args = drush_get_arguments();
+
+if ( ! isset($args[2]) ) {
+  drush_set_error('Usage: drush @<alias> scr menu-links-import.php <input-directory>');
+  exit();
+}
+
+if ( ! is_dir($args[2]) ) {
+  drush_set_error(t("Input directory @ip does not exist.", array('@ip' => $args[2])));
+  exit();
+}
+
+if ( ! file_exists($args[2] . '/menu-links.inc') ) {
+  drush_set_error(t("File @file does not exist.", array('@file' => $args[2] . '/menu-links.inc')));
+  exit();
+}
+
+if ( ! file_exists($args[2] . '/main_menu_menu_config.json') ) {
+  drush_set_error(t("File @file does not exist.", array('@file' => $args[2] . '/main_menu_menu_config.json')));
+  exit();
+}
+
+if ( ! file_exists($args[2] . '/main_menu_block_config.json') ) {
+  drush_set_error(t("File @file does not exist.", array('@file' => $args[2] . '/main_menu_block_config.json')));
+  exit();
+}
+
+import($args[2]);
+
+function import($path) {
+  $mappings = array();
+  include_once realpath($path) . '/menu-links.inc';
+  insert_menu_links($menu_links, $mappings);
+
+  $menu_config = json_decode(file_get_contents(realpath($path) . '/main_menu_menu_config.json'));
+  $block_config = file_get_contents(realpath($path) . '/main_menu_block_config.json');
+  save_tb_megamenus($menu_config, $block_config, $mappings);
+}
+
+function insert_menu_links(&$menu_links, &$mappings) {
+  drupal_set_message(t('Importing Menu Links'), 'ok');
+  foreach ( $menu_links as $mlid => &$menu_link ) {
+    if ( isset($menu_link['alias']) ) {
+      // fix link_path for imported tid or nid
+      $target = drupal_lookup_path('source', $menu_link['alias']);
+      $source = $menu_link['link_path'];
+      $menu_link['link_path'] = $target;
+
+      // Handle parent link mapping
+      if ( $menu_link['plid'] ) {
+        $plid = $menu_link['plid'];
+        $menu_link['plid'] = $mappings[$menu_link['plid']];
+      }
+
+      // Fix Identifier
+      if ( strpos($target, 'taxonomy/term/') === 0 ) {
+        $options = unserialize($menu_link['options']);
+        str_replace($source, $target, $options['identifier']);
+        $menu_link['options'] = serialize($options);
+      }
+      unset($menu_link['alias']);
+    }
+    $menu_link['mlid'] = 0;
+
+    $mappings[$mlid] = menu_link_save($menu_link);
+  }
+}
+
+function save_tb_megamenus($source_menu_config, $block_config, $mappings) {
+  drupal_set_message(t('Mapping Mega Menu'), 'ok');
+  $mapped_config = array();
+  foreach ( (array) $source_menu_config as $mlid => $config ) {
+    $config = $source_menu_config->$mlid;
+
+    $mapped_config[$mappings[$mlid]] = $config;
+    if ( ! empty($config->rows_content) ) {
+
+      foreach ( $config->rows_content as &$row ) {
+        foreach ( $row as &$column ) {
+          foreach ( $column->col_content as &$content) {
+            $content->mlid = $mappings[$content->mlid];
+          }
+        }
+      }
+
+
+    }
+  }
+
+  $menu_config = json_encode($mapped_config);
+
+  $tb_megamenu = db_select('tb_megamenus', 't')->fields('t')->condition('menu_name', 'main-menu')->execute()->fetchObject();
+  if($tb_megamenu) {
+    drupal_set_message(t('Update Existing Mega Menu'), 'ok');
+    db_update('tb_megamenus')->fields(array('menu_config' => $menu_config, 'block_config' => $block_config))->condition('menu_name', 'main-menu')->execute();
+  }
+  else {
+    drupal_set_message(t('Inserting New Mega Menu'), 'ok');
+    db_insert('tb_megamenus')->fields(array('menu_name' => 'main-menu', 'block_config' => $block_config, 'menu_config' => $menu_config))->execute();
+  }
+
+}
