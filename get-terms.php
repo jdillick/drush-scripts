4bbb0e763611511be4e2aea4f668c68b7a1e218b
John Dillick <john@dillick.us>
2014-09-03 11:16:19 -0400

    Started generalizing get-terms

--- get-terms.php
+++ get-terms.php
@@ -1,3 +1,19 @@
 <?php
-$vocabs = taxonomy_vocabulary_load_multiple(FALSE);
-print_r($vocabs);
+
+$args = drush_get_arguments();
+if ( ! isset($args[2]) ) {
+  drush_set_error('Usage: drush @<alias> scr get-terms.php <term names>');
+  exit();
+}
+
+$term_names = explode(',', trim($args[2]));
+$terms = array();
+foreach ( $term_names as $name ) {
+  $term_array = taxonomy_get_term_by_name($name);
+  foreach ( $term_array as $term ) {
+    $terms[$name]['path'] = 'taxonomy/term/' . $term->tid;
+    $terms[$name]['alias'] = drupal_get_path_alias($terms[$name]['path']);
+  }
+}
+
+print_r($terms);
