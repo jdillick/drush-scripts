0dfabbf18f61d36bcef8e42455806b91c64b586a
John Dillick <john@dillick.us>
2014-10-15 19:01:53 +0000

    Classroom tasks bash script, and updates to classroom content type cleanup.

---
+++ classroom/cleanup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Disabled/Enable Modules
+drush @classroom en -y hfc_environment_modules
+drush @classroom cc all; memflush --server=127.0.0.1; drush cc drush
+drush @classroom envm
+drush @classroom cc all; memflush --server=127.0.0.1; drush cc drush
+
+# Remove Unused Content Types
+drush @classroom scr delete-content-type.php act_hidden_pictures
+drush @classroom scr delete-content-type.php testimonial
+drush @classroom scr detach-doppels.php toolbox_content
+drush @classroom scr delete-content-type.php toolbox_content
+
+# Remove Unused Fields
+drush @classroom scr classroom/delete-unused-fields.php
+
+# Fix Doppelled Field Collections
+drush @classroom scr correct-renamed-doppel-field-collections.php classroom_product
+
+# Copy Id Fields to Doppel Types
+drush @classroom scr copy-identity-field-instances-to-doppels.php classroom_product,parent_teacher_guide,publication_club,publication_magazine,toolbox_media_content
+
+# Save all Doppel Types (causes doppel flattening)
+drush @classroom scr save-all-nodes-in-types.php classroom_product,parent_teacher_guide,publication_club,publication_magazine,toolbox_media_content
+
+# Remove Identity Content Types
+drush @classroom scr delete-content-type.php product
+drush @classroom scr delete-content-type.php textual_content
+drush @classroom scr delete-content-type.php club
+drush @classroom scr delete-content-type.php hub_magazine
+drush @classroom scr delete-content-type.php media_asset
+
+# Detach Doppel Profiles
+drush @classroom scr detach-doppels.php classroom_product,parent_teacher_guide,publication_club,publication_magazine,toolbox_media_content
+
+# Create FC Replacement for Parent Teacher Guide
+drush @classroom scr field-collections-to-content-types.php parent_teacher_guide
+
+# Copy FC Entities to Nodes for Parent Teacher Guide
+drush @classroom scr field-collection-entities-to-nodes.php parent_teacher_guide
+
+# Delete FC Entities and field instances for Parent Teacher Guide
+drush @classroom scr delete-field-collections.php parent_teacher_guide
--- classroom/delete-unused-fields.php
+++ classroom/delete-unused-fields.php
@@ -12,6 +12,35 @@ $cleanup = array(
   'classroom_product' => array(
     'field_class_google_custom_catego',
   ),
+  'toolbox_media_content' => array(
+    'field_banner_abstract',
+    'field_featured_on_toolbox',
+    'field_promo_hires_1_1',
+  ),
+  'media_asset' => array(
+    'field_hub_brand_alignment',
+    'field_hub_caption',
+    'field_hub_file_link',
+    'field_hub_id',
+    'field_hub_legacy_content_type',
+    'field_hub_legacy_url',
+    'field_hub_ma_brand_pillar',
+    'field_hub_ma_expiration_date',
+    'field_hub_ma_publish_date',
+    'field_hub_ma_tags',
+    'field_hub_ma_type_of_content',
+    'field_hub_me_age_high',
+    'field_hub_me_age_low',
+    'field_hub_me_grade_high',
+    'field_hub_me_grade_low',
+    'field_hub_nid',
+    'field_hub_related_content',
+    'field_hub_streaming_service_id',
+    'field_hub_stream_service',
+    'field_hub_target_audience',
+    'field_sites_allowed',
+    'field_toolbox_type_of_content',
+  ),
 );
 
 foreach ( $cleanup as $content_type => $fields ) {
